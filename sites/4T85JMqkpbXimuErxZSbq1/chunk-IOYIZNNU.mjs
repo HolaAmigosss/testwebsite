0733704d30f3e22e57a22af11156ea58a136fbab
HolaAmigosss <ponkin.matvey228@gmail.com>
2024-12-04 16:47:42 +0300

    Add files via upload

---
+++ sites/4T85JMqkpbXimuErxZSbq1/chunk-IOYIZNNU.mjs
@@ -0,0 +1,13 @@
+function t(e,a){return{customHTMLBodyStart:'<!-- Plugin: 4547ab --> <script>document.body.style.overscrollBehavior = "none";<\/script>',customHTMLHeadEnd:`<style>
+    ::selection {
+        background: rgba(255,255,255,0.2);
+        color: #fff;
+    }
+    @media screen and (max-width: 768px) {
+        input, textarea, select {
+            font-size: 16px !important;
+        }
+    }
+<style>
+                                            </head></style>`,description:"welcome, degen.",favicon:"https://framerusercontent.com/assets/Gydxc7RQVAqKGDMcDdFSEGcn8XM.jpg",robots:"max-image-preview:large",socialImage:"https://framerusercontent.com/assets/BZIdGEh8V43uYDfUH7r9WUGzOF0.png",title:"mindbath"}}export{t as a};
+//# sourceMappingURL=chunk-IOYIZNNU.mjs.map
